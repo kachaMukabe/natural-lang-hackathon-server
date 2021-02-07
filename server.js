5a0fb1fd3f08218c37e39f926661d6807477ce04
Kacha Mukabe <kachamukabe@gmail.com>
2021-02-07 14:48:23 +0200

    added call to expert ai

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express"),
     cors = require('cors'),
     atob = require('atob'),
     blob = require('node-blob'),
+    fetch = require('node-fetch'),
     { Octokit } = require("@octokit/core"),
     bodyParser = require('body-parser');
 
@@ -19,6 +20,35 @@ const repoDetails = (url) => {
 
     return {user: user, repo: repo}
 }
+
+const process_text = async (readme) => {
+    const token_resp = await fetch("https://developer.expert.ai/oauth2/token", {
+    method: 'POST',
+    body: JSON.stringify({
+        "username": process.env.EAI_USERNAME,
+        "password": process.env.EAI_PASSWORD
+    }),
+    headers: {'Content-Type': 'application/json'}
+    })
+    token = await token_resp.text()
+    auth = `Bearer ${token}`
+    console.log(auth)
+    const response = await fetch("https://nlapi.expert.ai/v2/analyze/standard/en", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        Authorization: auth,
+        body: JSON.stringify({
+            "document": {
+                "text": readme
+            }
+        })
+    })
+
+    analyzed_text = await response
+    console.log(analyzed_text)
+}
+
+
 app.post('/repo', async (req, res)=>{
     if(!req.body.repoUrl){
         return res.status(400).send({success: "false", message: "Github repository url is required"});
@@ -30,8 +60,19 @@ app.post('/repo', async (req, res)=>{
         owner: details.user,
         repo: details.repo
     })
-    console.log(response)
+    // console.log(response)
     let decodedreadme = atob(response.data.content)
+    await process_text(decodedreadme)
+    // const token = await fetch("https://developer.expert.ai/oauth2/token", {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         "username": "kmukabe@gmail.com",
+    //         "password": "Watashiwabaka8*"
+    //     }),
+    //     headers: {'Content-Type': 'application/json'}
+    // })
+    
+    // console.log(token)
     return res.status(201).send({success: true, readme: decodedreadme, shortened: "Shortened"})
 });
 
